ca8d4d514deb7d54738e19ace6fec651a5df9f8b
spidomisiek88 <michal.pajak88@gmail.com>
2019-07-25 13:54:30 +0200

    main class

--- src/main/java/pl/michalPajak/averageNumberOfVowelsInWord/AverageNumberOfVowelsInWord.java
+++ src/main/java/pl/michalPajak/averageNumberOfVowelsInWord/AverageNumberOfVowelsInWord.java
@@ -1,7 +1,14 @@
 package pl.michalPajak.averageNumberOfVowelsInWord;
 
+import java.nio.file.Paths;
+
 public class AverageNumberOfVowelsInWord {
     public static void main(String[] args) {
 
+        String inputFile = InputOutputOperation
+                .readFileToString(Paths
+                        .get("C:\\Users\\Spido\\IdeaProjects\\averageNumberOfVowelsInWord\\INPUT.txt"));
+
+        
     }
 }
